3e39388e78eea7d6d854049384885635aae9cb20
Saddique Shafi <saddiqueashafi@gmail.com>
2020-05-08 17:26:42 -0400

    Added (edge)padding to sequences

--- data.py
+++ data.py
@@ -38,6 +38,14 @@ class Data():
             if row[0] == datasetType:
                 count += 1
         return count
+
+    def getMaxFrameCount(self):
+        maxFrameCount = 0
+        for row in self.data:
+            if int(row[3]) > maxFrameCount:
+                maxFrameCount = int(row[3])
+        return maxFrameCount
+
     
     # returns a list of paths to frames for a video
     @staticmethod
@@ -47,6 +55,6 @@ class Data():
         frames        = sorted(framesPath.glob(videoFilename + '*')) # sorted list so that the frames are in 'sequential' order
         return frames
 
-##if __name__ == '__main__':
-##    d = Data()
-##    print(d.getDatasetCount("Test"))
+if __name__ == '__main__':
+    d = Data()
+    print(d.getMaxFrameCount())
